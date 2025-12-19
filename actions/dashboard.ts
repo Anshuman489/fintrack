e655c75f69c557e6256d177ad00e87c539feacb3
Anshuman489 <anshumanpadhi05@gmail.com>
2025-12-20 03:27:15 +0530

    added dashboard accounts drawer and create account logic

---
+++ actions/dashboard.ts
@@ -0,0 +1,111 @@
+"use server";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { AccountType } from "@/generated/prisma/wasm";
+import { revalidatePath } from "next/cache";
+
+type CreateAccountData = {
+  name: string;
+  type: AccountType;
+  balance: string;
+  isDefault: boolean;
+};
+
+const serializeTransaction = (obj: any) => {
+  const serialized = { ...obj };
+
+  if (obj.balance) {
+    serialized.balance = obj.balance.toNumber();
+  }
+
+  if (obj.amount) {
+    serialized.amount = obj.amount.toNumber();
+  }
+
+  return serialized;
+};
+
+export async function createAccount(data: CreateAccountData) {
+  try {
+    const { userId } = await auth();
+    if (!userId) throw new Error("Unauthorized");
+
+    const user = await prisma.user.findUnique({
+      where: { clerkUserId: userId },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // Convert balance to float before saving
+
+    const balanceFloat = parseFloat(data.balance);
+
+    if (isNaN(balanceFloat)) {
+      throw new Error("Invalid balance amount");
+    }
+
+    // Check if this is the user's first account
+    const existingAccounts = await prisma.account.findMany({
+      where: { userId: user.id },
+    });
+
+    const shouldBeDefault =
+      existingAccounts.length === 0 ? true : data.isDefault;
+
+    //if this account should be default, unset other default accounts
+    if (shouldBeDefault) {
+      await prisma.account.updateMany({
+        where: { userId: user.id, isDefault: true },
+        data: { isDefault: false },
+      });
+    }
+
+    const account = await prisma.account.create({
+      data: {
+        ...data,
+        balance: balanceFloat,
+        userId: user.id,
+        isDefault: shouldBeDefault,
+      },
+    });
+
+    const serializedAccount = serializeTransaction(account);
+
+    revalidatePath("/dashboard");
+
+    return { success: true, data: serializedAccount };
+  } catch (error) {
+    throw new Error((error as Error).message);
+  }
+}
+
+export async function getUserAccounts() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await prisma.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const accounts = await prisma.account.findMany({
+    where: { userId: user.id },
+    orderBy: { createdAt: "desc" },
+    include: {
+      _count: {
+        select: {
+          transactions: true,
+        },
+      },
+    },
+  });
+
+  const serializedAccount = accounts.map(serializeTransaction);
+
+  return serializedAccount;
+}
--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import CreateAccountDrawer from "@/components/create-account-drawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import { getUserAccounts } from "@/actions/dashboard";
+import AccountCard from "./_components/account-card";
 
-const Dashboard = () => {
+const DashboardPage = async () => {
+  const accounts = await getUserAccounts();
+
+  // console.log(accounts);
   return (
-    <div>
-      Dashboard Page
-    </div>
-  )
-}
+    <div className="px-5">
+      {/* Budget Progress */}
 
-export default Dashboard
+      {/* Overview */}
+
+      {/* Accounts Grid */}
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <CreateAccountDrawer>
+          <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
+            <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Create New Account</p>
+            </CardContent>
+          </Card>
+        </CreateAccountDrawer>
+
+        {accounts.length > 0 &&
+          accounts?.map((account) => {
+            return <AccountCard key={account.id} account={account} />;
+          })}
+      </div>
+    </div>
+  );
+};
+
+export default DashboardPage;
