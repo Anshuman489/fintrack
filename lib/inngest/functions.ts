d4ae86e3c0e16cfea772e9e54bdd1d48847accea
Anshuman489 <anshumanpadhi05@gmail.com>
2025-12-22 18:22:10 +0530

    added cron job for budget alert - inngest

---
+++ lib/inngest/functions.ts
@@ -0,0 +1,84 @@
+import { inngest } from "./client";
+import prisma from "@/lib/prisma";
+
+export const checkBudgetAlert = inngest.createFunction(
+  { id: "check-budget-alerts", name: "Check Budget Alerts" },
+  { cron: "0 */6 * * *" },
+  async ({ step }) => {
+    const budgets = await step.run("fetch-budget", async () => {
+      return await prisma.budget.findMany({
+        include: {
+          user: {
+            include: {
+              accounts: {
+                where: {
+                  isDefault: true,
+                },
+              },
+            },
+          },
+        },
+      });
+    });
+
+    for (const budget of budgets) {
+      const defaultAccount = budget.user.accounts[0];
+      if (!defaultAccount) continue; //skip if no default account
+
+      await step.run(`check-budget-${budget.id}`, async () => {
+        const currentDate = new Date();
+        const startOfMonth = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth(),
+          1
+        );
+
+        const endOfMonth = new Date(
+          currentDate.getFullYear(),
+          currentDate.getMonth() + 1,
+          0
+        );
+
+        const expenses = await prisma.transaction.aggregate({
+          where: {
+            userId: budget.userId,
+            accountId: defaultAccount.id,
+            type: "EXPENSE",
+            date: {
+              gte: startOfMonth,
+              lte: endOfMonth,
+            },
+          },
+          _sum: {
+            amount: true,
+          },
+        });
+
+        const totalExpenses = expenses._sum.amount?.toNumber() || 0;
+        const budgetAmount = parseFloat(budget.amount);
+        const percentageUsed = (totalExpenses / budgetAmount) * 100;
+
+        if (
+          percentageUsed >= 80 &&
+          (!budget.lastAlertSent ||
+            isNewMonth(new Date(budget.lastAlertSent), new Date()))
+        ) {
+          //send email
+
+          //update lastAlertSent
+          await prisma.budget.update({
+            where: { id: budget.id },
+            data: { lastAlertSent: new Date() },
+          });
+        }
+      });
+    }
+  }
+);
+
+function isNewMonth(lastAlertSent: Date, currentDate: Date) {
+  return (
+    lastAlertSent.getMonth() !== currentDate.getMonth() ||
+    lastAlertSent.getFullYear() !== currentDate.getFullYear()
+  );
+}
